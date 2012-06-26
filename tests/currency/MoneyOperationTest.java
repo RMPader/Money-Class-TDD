303c24de266cb5c2a2287ac9080484cc70b57c22
Reynald Pader <reynald.pader@orangeandbronze.com>
2012-06-26 09:57:40 +0800

    added equality tests

--- tests/currency/MoneyOperationTest.java
+++ tests/currency/MoneyOperationTest.java
@@ -1,5 +1,6 @@
 package currency;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,6 +17,61 @@ public class MoneyOperationTest {
 	assertTrue(php.toString().equals("PHP 1.00"));
 	assertTrue(usd.toString().equals("USD 0.01"));
 	assertTrue(eur.toString().equals("EUR -1.00"));
+    }
+    
+    @Test
+    public void nonEqualityTests(){
+	Money php1 = new Money(Currency.PHP, 14, 0);
+	Money php2 = new Money(Currency.PHP, 14, 2);
+	assertFalse(php1.equals(php2));
+	
+	Money eur1 = new Money(Currency.EUR, 0, 23);
+	Money eur2 = new Money(Currency.EUR, 0, 2);
+	assertFalse(eur1.equals(eur2));
+	
+	Money usd1 = new Money(Currency.USD, 1, 55);
+	Money usd2 = new Money(Currency.USD, 2, 55);
+	assertFalse(usd1.equals(usd2));
+		
+	Money m1 = new Money(Currency.USD, 1, 11);
+	Money m2 = new Money(Currency.PHP, 1, 11);
+	assertFalse(m1.equals(m2));
+    }
+    
+    @Test
+    public void moneyEqualsNull() {
+	Money money = new Money(Currency.USD, 1, 0);
+	assertFalse(money.equals(null));
+    }
+
+    @Test
+    public void moneyEqualsReflexive() {
+	Money money = new Money(Currency.USD, 1, 0);
+	assertTrue(money.equals(money) && money.hashCode() == money.hashCode());
+
+	money = new Money(Currency.PHP, 1, 10);
+	assertTrue(money.equals(money) && money.hashCode() == money.hashCode());
+
+	money = new Money(Currency.USD, 10, 1);
+	assertTrue(money.equals(money) && money.hashCode() == money.hashCode());
+    }
+
+    @Test
+    public void moneyEqualsSymmetric() {
+	Money money1 = (Money) new Money(Currency.EUR, 12, 34);
+	Money money2 = (Money) new Money(Currency.EUR, 12, 34);
+	assertTrue((money1.equals(money2) && money2.equals(money1))
+		&& (money1.hashCode() == money2.hashCode()));
+    }
+    
+    @Test
+    public void moneyEqualsTransitive() {
+	Money money1 = (Money) new Money(Currency.USD, 987, 65);
+	Money money2 = (Money) new Money(Currency.USD, 987, 65);
+	Money money3 = (Money) new Money(Currency.USD, 987, 65);
+	assertTrue(money1.equals(money2) && money2.equals(money3)
+		&& money1.equals(money3)
+		&& money1.hashCode() == money3.hashCode());
     }
     
     @Test(expected=ArithmeticException.class)
@@ -45,3 +101,4 @@ public class MoneyOperationTest {
 	assertTrue(factor.multiply(1).equals(assumedResult));
     }
 }
+
