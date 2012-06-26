c04c8603b98532b36100ca30cc83db487975334f
clivelim <Clive.lim@orangeandbronze.com>
2012-06-26 09:59:49 +0800

    added add and subtract

--- src/currency/Money.java
+++ src/currency/Money.java
@@ -12,13 +12,12 @@ public class Money {
     private final boolean hasNegativeValue;
     private final Currency currency;
 
-    public Money(Currency currency, int wholeNumber, int decimalNumber)
-	    throws InvalidMoneyValueException {
+    public Money(Currency currency, int wholeNumber, int decimalNumber) throws InvalidMoneyValueException {
 	if (wholeNumber == 0) {
 	    hasNegativeValue = (decimalNumber < 0) ? true : false;
 	} else if (wholeNumber < 0) {
 	    hasNegativeValue = true;
-	} else{
+	} else {
 	    hasNegativeValue = false;
 	}
 	this.currency = currency;
@@ -37,28 +36,57 @@ public class Money {
 	double productDecimal = ((double) decimalNumber) / dividend;
 	return abracadabra(currency, productWholeNumber, productDecimal);
     }
-    
-    //TODO rename this muthafuka
-    private static Money abracadabra(Currency currency, double productWholeNumber, double productDecimal){
+
+    // TODO rename this muthafuka
+    private static Money abracadabra(Currency currency, double productWholeNumber, double productDecimal) {
 	double product = productWholeNumber + (productDecimal / 100);
 	StringBuilder sb = new StringBuilder(Double.toString(product));
 	int index = sb.indexOf(".");
 	index = index + 3;
 	if (Integer.parseInt(sb.substring(index, index + 1)) >= 5) {
-	    sb.setCharAt(index - 1, Character.forDigit(
-		    Integer.parseInt(sb.substring(index - 1, index)) + 1, 10));
+	    sb.setCharAt(index - 1, Character.forDigit(Integer.parseInt(sb.substring(index - 1, index)) + 1, 10));
 	}
 	sb.delete(index, sb.length());
 	int whole = Integer.parseInt(sb.substring(0, sb.indexOf(".")));
-	int decimal = Integer.parseInt(sb.substring(sb.indexOf(".") + 1,
-		sb.length()));
+	int decimal = Integer.parseInt(sb.substring(sb.indexOf(".") + 1, sb.length()));
 	return new Money(currency, whole, decimal);
     }
 
-  //////
-   ///PUT YOUR ADD/SUBTRACT STUFF HERE
-  //////
-    
+    // ////
+    // /PUT YOUR ADD/SUBTRACT STUFF HERE
+    // ////
+    public Money add(Money addend) {
+	checkisSameCurrency(addend);
+	int wholeNumber = this.wholeNumber + addend.wholeNumber;
+	int decimalNumber = this.decimalNumber + addend.decimalNumber;
+	if (decimalNumber >= 100) {
+	    wholeNumber++;
+	    decimalNumber %= 100;
+	}
+	return new Money(currency, wholeNumber, decimalNumber);
+    }
+
+    public void checkisSameCurrency(Money money) {
+	if (currency != money.currency) {
+	    String message = concatAll("cannot perform operation on ", currency.toString(), " and ", money.currency.toString());
+	    throw new IncompatibleCurrencyException(message);
+	}
+    }
+
+    public Money subtract(Money subtrahend) {
+	checkisSameCurrency(subtrahend);
+	int wholeNumber = this.wholeNumber - subtrahend.wholeNumber;
+	int decimalNumber = this.decimalNumber - subtrahend.decimalNumber;
+	if (decimalNumber < 0) {
+	    int borrow = 100;
+	    if (this.decimalNumber < 10)
+		borrow = 10;
+	    wholeNumber--;
+	    decimalNumber = Math.abs(decimalNumber + borrow);
+	}
+	return new Money(currency, wholeNumber, decimalNumber);
+    }
+
     private static String concatAll(String... strings) {
 	StringBuilder newString = new StringBuilder();
 	for (String s : strings) {
@@ -72,17 +100,14 @@ public class Money {
     }
 
     public BigDecimal getValue() {
-	String valueString = concatAll(Integer.toString(wholeNumber), ".",
-		Integer.toString(decimalNumber));
+	String valueString = concatAll(Integer.toString(wholeNumber), ".", Integer.toString(decimalNumber));
 	BigDecimal bd = new BigDecimal(valueString);
 	return bd;
     }
 
     @Override
     public String toString() {
-	String toReturn = concatAll(currency.toString(), " ",
-		Integer.toString(wholeNumber), ".",
-		Integer.toString(decimalNumber));
+	String toReturn = concatAll(currency.toString(), " ", Integer.toString(wholeNumber), ".", Integer.toString(decimalNumber));
 	return toReturn;
     }
 
@@ -90,8 +115,7 @@ public class Money {
     public int hashCode() {
 	final int prime = 31;
 	int result = 1;
-	result = prime * result
-		+ ((currency == null) ? 0 : currency.hashCode());
+	result = prime * result + ((currency == null) ? 0 : currency.hashCode());
 	result = prime * result + decimalNumber;
 	result = prime * result + (hasNegativeValue ? 1231 : 1237);
 	result = prime * result + wholeNumber;
