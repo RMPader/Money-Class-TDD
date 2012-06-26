4e12c6aad0376551b69d845f859248b17b6eb8c2
clivelim <Clive.lim@orangeandbronze.com>
2012-06-26 11:30:54 +0800

    fixed test cases

--- tests/currency/MoneyOperationTest.java
+++ tests/currency/MoneyOperationTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import currency.Currency;
+import currency.Money;
 import currency.exceptions.IncompatibleCurrencyException;
 
 public class MoneyOperationTest {
